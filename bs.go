60ed334480e050ffca9918164beb1808b5b02854
Alireza <26859725+tralireza@users.noreply.github.com>
2022-06-01 18:10:00 +0100

    Binary Search 786m K-th Smallest Prime Fraction

--- bs.go
+++ bs.go
@@ -1,8 +1,52 @@
 package BinS
 
-import "log"
+import (
+	"log"
+)
 
 func init() {
 	log.SetFlags(0)
 	log.Print("> Binary Search")
+}
+
+type R786 struct{ n, d int }
+
+func (o R786) Ratio(arr []int) float64 { return float64(arr[o.n]) / float64(arr[o.d]) }
+
+// 786m K-th Smallest Prime Fraction
+func kthSmallestPrimeFraction(arr []int, k int) []int {
+	type R = R786
+
+	l, r := float64(0), float64(1)
+	for l < r {
+		m := (l + r) / 2
+
+		x, xRatio := 0, R{0, len(arr) - 1}
+		d := 1
+		for n := 0; n < len(arr)-1; n++ {
+			for d < len(arr) && m <= (R{n, d}).Ratio(arr) {
+				d++
+			}
+			if d == len(arr) {
+				break
+			}
+
+			x += len(arr) - d
+			if xRatio.n == -1 && xRatio.d == -1 || xRatio.Ratio(arr) < (R{n, d}).Ratio(arr) {
+				xRatio = R{n, d}
+			}
+		}
+		log.Printf("l:%g   %d %d   m: %g   r:%g   -> %v", l, x, k, m, r, xRatio)
+
+		if k == x {
+			return []int{arr[xRatio.n], arr[xRatio.d]}
+		}
+		if x > k {
+			r = m
+		} else {
+			l = m
+		}
+	}
+
+	return []int{}
 }
--- bs_test.go
+++ bs_test.go
@@ -1,3 +1,16 @@
 package BinS
 
+import (
+	"log"
+	"testing"
+)
+
 func init() {}
+
+// 786m K-th Smallest Prime Fraction
+func Test786(t *testing.T) {
+	log.Print("[2 5] ?= ", kthSmallestPrimeFraction([]int{1, 2, 3, 5}, 3))
+	log.Print("[1 7] ?= ", kthSmallestPrimeFraction([]int{1, 7}, 1))
+	log.Print("[1 47] ?= ", kthSmallestPrimeFraction([]int{1, 29, 47}, 1))
+	log.Print("[13 17] ?= ", kthSmallestPrimeFraction([]int{1, 13, 17, 59}, 6))
+}
